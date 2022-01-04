884975378bdab366ba8c595ac9af044b06e209f3
Nicholas Westerhausen <nwesterhausen@ntsupply.com>
2022-01-04 16:47:22 -0500

    keep track of when we reach the final file and don't put a comma

--- run-rust.ps1
+++ run-rust.ps1
@@ -1,17 +1,20 @@
 # $saveFolder = "C:\Users\nwest\scoop\apps\dwarf-fortress\0.47.05\data\save\region1\raw";
-$saveFolder = "C:\Users\nwesterhausen\scoop\apps\dwarf-fortress\0.47.05\data\save\region1\raw";
+$saveFolder = "C:\Users\nwesterhausen\Sync\Dwarf Fortress Files\DFSaves\region8\raw";
 
-$raws = Get-ChildItem -Path $saveFolder -Include *.txt -Recurse | Where-Object { ! $_.PSIsContainer }
+$raws = Get-ChildItem -Path $saveFolder -Include *creature*.txt -Recurse | Where-Object { ! $_.PSIsContainer }
 
 $stepCounter = 0;
 
 Set-Content -Path .\out.json "["
-foreach ($file in $raws) {
+for ($i = 0; $i -lt $raws.Length; $i++) {
+    $file = $raws[$i];
     Write-Progress -Activity "Parsing Raws" -Status (Get-Item $file ).Name -PercentComplete ((($stepCounter++) / $raws.Length) * 100)
     $parsed = target\debug\df-raw-lookup.exe $file
-    if ($parsed.length -gt 0) {
+    if ($parsed.length -gt 1) {
         Add-Content -Path .\out.json $parsed -NoNewline
-        Add-Content -Path .\out.json "," -NoNewline
+        if ($i + 1 -lt $raws.Length) {
+            Add-Content -Path .\out.json "," -NoNewline
+        }
     }
 }
 Add-Content -Path .\out.json "]" -NoNewline
