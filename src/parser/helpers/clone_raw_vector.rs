9a70e6590759eb64ed4379dd05a945d3ef6fcb3c
Nicholas Westerhausen <nwesterhausen@gmail.com>
2023-10-21 07:05:26 -0400

    fix: page number - 1

--- src/parser/helpers/clone_raw_vector.rs
+++ src/parser/helpers/clone_raw_vector.rs
@@ -117,15 +117,34 @@ pub fn with_purge(
     new_raws
 }
 
+/// The function `with_limit_and_page` takes a slice of `RawObject` objects, a limit, and a page number,
+/// and returns a new vector containing a subset of the original objects based on the limit and page
+/// number.
+///
+/// Arguments:
+///
+/// * `all_raws`: A slice of boxed objects that implement the `RawObject` trait.
+/// * `limit`: The `limit` parameter specifies the maximum number of items to be included in each page
+/// of the result.
+/// * `page`: The `page` parameter represents the page number of the data you want to retrieve. It is
+/// used to calculate the starting and ending positions of the data based on the `limit` parameter. The
+/// first page is represented by page number 1, so if you want to retrieve data from the first page
+///
+/// Returns:
+///
+/// a vector of boxed dynamic objects (`Vec<Box<dyn RawObject>>`).
 pub fn with_limit_and_page(
     all_raws: &[Box<dyn RawObject>],
     limit: usize,
     page: usize,
 ) -> Vec<Box<dyn RawObject>> {
     let mut new_raws: Vec<Box<dyn RawObject>> = Vec::new();
-
+    // Page 0 is the first page, so we need to subtract 1 from the page number
+    let page = page - 1;
     let start = limit * page;
     let end = start + limit;
+
+    log::info!("with_limit_and_page start: {start}, end: {end}, page: {page}");
 
     for (pos, raw) in all_raws.iter().enumerate() {
         if pos >= start && pos < end {
