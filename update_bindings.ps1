2b57ff6b1d8a7499be5fffda3808e115f39adbb1
Nick Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2024-05-29 12:00:38 -0400

    chore: update to beta version of specta

--- update_bindings.ps1
+++ update_bindings.ps1
@@ -1,3 +1,4 @@
 Remove-Item '.\lib\bindings\*' -Force
 cargo test --all-features
-bunx @biomejs/biome format --write lib/bindings/*.ts
+bunx @biomejs/biome format --write "./lib/bindings/DFRawJson.d.ts"
+bunx @biomejs/biome format --write "./lib/bindings/DFRawJson-Tauri.d.ts"
